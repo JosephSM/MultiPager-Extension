7f86e1e20bb0aa427304a213006b0c5494952315
Joseph Schwartz-Messing <josephsmy94@gmail.com>
2025-03-31 11:08:26 -0400

    mishnah berurah fix

--- scripts/mishnahberurahyomishiur.js
+++ scripts/mishnahberurahyomishiur.js
@@ -1,2 +1,2 @@
-v = document.querySelector("video");
-v.style = "width:100%; margin:0px; height:35px;";
+// v = document.querySelector("video");
+// v.style = "width:100%; margin:0px; height:35px;";
